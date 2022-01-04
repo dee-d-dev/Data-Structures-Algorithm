e12cb9d83c249a0c8f2f54381551f1e00604ae2a
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-01-04 20:57:42 +0100

    Linked List

--- Linked_Lists/linked_list.js
+++ Linked_Lists/linked_list.js
@@ -12,6 +12,12 @@
 //     }
 // }
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -22,29 +28,63 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
-    this.head.next = newNode;
+    this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
 
   prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
     const newNode = {
       value: value,
       next: null,
     };
-    newNode.next = this.head;
-    this.head = newNode;
+
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next
+    leader.next = newNode
+    newNode.next = holdingPointer
     this.length++
-    return this
+    return this.printList()
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter != index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode
   }
 }
 const myLinkedList = new LinkedList(10);
 // console.log(myLinkedList);
 console.log(myLinkedList.append(5));
+console.log(myLinkedList.append(15));
 console.log(myLinkedList.prepend(1));
+console.log(myLinkedList.insert(2,99))
+// console.log(myLinkedList.printList());
