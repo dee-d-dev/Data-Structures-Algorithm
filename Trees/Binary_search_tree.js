41cd9ea9a35db323e6bb2f80301906bcc42e2197
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-01-15 02:06:47 +0100

    implementing Binary seacrh trees

---
+++ Trees/Binary_search_tree.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root == null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  remove() {}
+
+  lookUp() {}
+}
+
+const BST = new BinarySearchTree();
+BST.insert(5);
+BST.insert(3);
+BST.insert(8);
+BST.insert(4);
+BST.insert(6);
+console.log(BST.insert(9));
+
+//     5
+// 3      8
+//2  4  6    9
