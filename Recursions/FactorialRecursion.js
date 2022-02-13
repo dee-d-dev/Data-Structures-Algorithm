28a0bebb1716fa3848eeb273609a26bac214ecd3
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-13 09:04:15 +0100

    implementing factorial solution with recursion [iteratively and recursively]

--- Recursions/FactorialRecursion.js
+++ Recursions/FactorialRecursion.js
@@ -1,10 +1,21 @@
-function FactorialRecursion(number) {
-  let answer = 1;
-  for (i = 1; i <= number; i++) {
-    answer = i * answer;
+// function FactorialRecursionIterative(number) {
+//   let answer = 1;
+//   for (i = 1; i <= number; i++) {
+//     answer = i * answer;
+//   }
+
+//   console.log(answer);
+// }
+
+function FactorialRecursionRecursive(number) {
+  if (number === 2) {
+    return 2;
   }
 
+  let answer = number * FactorialRecursionRecursive(number - 1);
   console.log(answer);
+  return answer;
 }
 
-FactorialRecursion(2);
+// FactorialRecursionIterative(5);
+FactorialRecursionRecursive(6);
