c3df873386b1e27a05f9175bac6f84cfd7395993
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-24 04:49:37 +0100

    Using dynamic programming

--- Recursions/Fibonacci.js
+++ Recursions/Fibonacci.js
@@ -9,15 +9,35 @@ function fibonacciRecursive(n) {
 }
 
 function fibonacciIterative(n) {
-
   let arr = [0, 1];
-  for (i = 2; i < n+1; i++) {
+  for (i = 2; i < n + 1; i++) {
     arr.push(arr[i - 1] + arr[i - 2]);
   }
 
   return arr;
-  
 }
 
+// fibonacciIterative();
 
-fibonacciIterative();
+//Using DYNAMIC PROGRAMMING
+let calculations = 0;
+function fibonacciMaster() {
+  let cache = {};
+  return function fib(n) {
+    calculations++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+}
+
+// console.log(fibonacciMaster(10))
+fibonacciMaster(10);
+console.log(`calculation are ${calculations}`);
