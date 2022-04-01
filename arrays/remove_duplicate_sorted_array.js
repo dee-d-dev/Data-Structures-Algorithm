9dda172a4bf28e2970d723c3508041f5cbe8d0ff
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-04-01 09:56:09 +0100

    remove duplicated sorted arrays

--- arrays/remove_duplicate_sorted_array.js
+++ arrays/remove_duplicate_sorted_array.js
@@ -1,17 +1,25 @@
-let  removeDuplicates = function (nums) {
-  let l = 0;
+// let  removeDuplicates = function (nums) {
+//   let l = 0;
 
-  // right pointer to loop through the array
-  for (let r = 1; r < nums.length; r++) {
-    if (nums[l] === nums[r]) {
-      // go to next iteration
-      continue;
-    }
+//   // right pointer to loop through the array
+//   for (let r = 1; r < nums.length; r++) {
+//     if (nums[l] === nums[r]) {
+//       // go to next iteration
+//       continue;
+//     }
 
-    //update the array
-    nums[++l] = nums[r];
+//     //update the array
+//     nums[++l] = nums[r];
+//   }
+//   console.log(l + 1); // since l start counting from 0
+// };
+
+let removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums.splice(i, nums.lastIndexOf(nums[i]) - i);
   }
-  console.log(l + 1); // since l start counting from 0
+
+  console.log(nums.length);
 };
 
-removeDuplicates([1,1,2])
+removeDuplicates([1, 1, 2]);
