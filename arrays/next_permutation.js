338acc00bac7f32a7e2457eaa96282bcb8511666
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-04-04 05:43:58 +0100

    next permutation

--- arrays/next_permutation.js
+++ arrays/next_permutation.js
@@ -1,41 +1,74 @@
 function nextPermuatation(nums) {
-  const len = nums.length - 1;
-  //stores the first index and second index for swapping.
-  let index;
-  let secIndex;
+  //[1,5,3]
+  //   const len = nums.length - 1;
+  //   //stores the first index and second index for swapping.
+  //   let index;
+  //   let secIndex;
 
-  //get the first index
-  for (index = len - 1; index >= 0; index--) {
-    if (nums[index] < nums[index + 1]) {
+  //   //get the first index
+  //   for (index = len - 1; index >= 0; index--) {
+  //     if (nums[index] < nums[index + 1]) {
+  //       break;
+  //     }
+  //   }
+  //   //if we don't find first index we don't find second index. we just reverse nums arr.
+  //   //that means if we get [3,2,1] as input then our next permutation is [1,2,3].
+  //   if (index < 0) {
+  //     nums.reverse();
+  //   } else {
+  //     //find the second index from last.
+  //     //if we find a value from back which is greater than value of index obtained.
+  //     for (secIndex = len; secIndex > index; secIndex--) {
+  //       if (nums[secIndex] > nums[index]) {
+  //         break;
+  //       }
+  //     }
+  //     //swap values of index and secondindex.
+  //     [nums[index], nums[secIndex]] = [nums[secIndex], nums[index]];
+
+  //     //reverse values after the index value till last.
+  //     let low = index + 1;
+  //     let high = len;
+  //     while (low < high) {
+  //       [nums[low], nums[high]] = [nums[high], nums[low]];
+  //       low++;
+  //       high--;
+  //     }
+  //   }
+
+  let len = nums.length - 1;
+
+  let idx;
+  let secIdx;
+
+  for (idx = len - 1; idx >= 0; idx--) {
+    if (nums[idx] < nums[idx + 1]) {
       break;
     }
   }
-  //if we don't find first index we don't find second index. we just reverse nums arr.
-  //that means if we get [3,2,1] as input then our next permutation is [1,2,3].
-  if (index < 0) {
-    nums.reverse();
+  if (idx < 0) {
+    console.log(nums.reverse());
   } else {
-    //find the second index from last.
-    //if we find a value from back which is greater than value of index optained.
-    for (secIndex = len; secIndex > index; secIndex--) {
-      if (nums[secIndex] > nums[index]) {
+    for (secIdx = len; secIdx > idx; secIdx--) {
+      if (nums[secIdx] > nums[idx]) {
         break;
       }
     }
-    //swap values of index and secondindex.
-    [nums[index], nums[secIndex]] = [nums[secIndex], nums[index]];
+  }
 
-    //reverse values after the index value till last.
-    let low = index + 1;
-    let high = len;
-    while (low < high) {
-      [nums[low], nums[high]] = [nums[high], nums[low]];
-      low++;
-      high--;
-    }
+  [nums[idx], nums[idx + 1]] = [nums[idx + 1], nums[idx]];
+
+  let low = idx + 1;
+  let high = len;
+
+  while (low < high) {
+    [nums[low], nums[high]] = [nums[high], nums[low]];
+
+    low++;
+    high--;
   }
 
   console.log(nums);
 }
 
-nextPermuatation([1, 3, 2]);
+nextPermuatation([1, 7, 7]);
