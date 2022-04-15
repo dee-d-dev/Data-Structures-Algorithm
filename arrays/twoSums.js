a3d539b7470f3ab95ffd8c27754012836f9b49d7
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-04-15 11:00:23 +0100

    two sums

--- arrays/twoSums.js
+++ arrays/twoSums.js
@@ -12,7 +12,7 @@ let twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     let index = nums.indexOf(target - nums[i], i + 1);
     if (index !== -1) {
-      return [i, index];
+      console.log([i, index]);
     }
   }
 };
