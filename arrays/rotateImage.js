ce338f54002ac68763fb7ac05b26d6307cd52d32
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-04-18 18:12:31 +0100

    rotate arrays

---
+++ arrays/rotateImage.js
@@ -0,0 +1,23 @@
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+let rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix[i].length; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].reverse();
+}
+console.log(matrix)
+};
+
+rotate([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
