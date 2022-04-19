71db601350db2785f5ed2d337b6dc1a7b78e8391
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-04-19 08:31:58 +0100

    rotate image

--- arrays/rotateImage.js
+++ arrays/rotateImage.js
@@ -8,12 +8,50 @@ let rotate = function (matrix) {
       let temp = matrix[i][j];
       matrix[i][j] = matrix[j][i];
       matrix[j][i] = temp;
+
+      /*
+    
+        example=[
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ]
+
+        final=[
+          [7, 4, 1],
+          [8, 5, 2],
+          [9, 6, 3],
+        ]
+
+        transform = [
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 6, 9],
+        ]
+       at i=0 j=1, matrix[i][j] = matrix[0][1]
+       temp = matrix[0][1]; //2
+       matrix[i][j] = matrix[j][i]; //4
+       matrix[j][i] = temp; //2
+
+       at i=0 j=2, matrix[i][j] = matrix[0][2]
+       temp = matrix[0][2]; //3
+       matrix[i][j] = matrix[j][i]; //7
+       matrix[j][i] = temp; //3
+
+       at i=1 j=2, matrix[i][j] = matrix[1][2]
+       temp = matrix[1][2]; //6
+       matrix[i][j] = matrix[j][i]; //8
+       matrix[j][i] = temp; //6
+
+       //reverse each row
+       i<matrix.length
+      */
     }
   }
   for (let i = 0; i < matrix.length; i++) {
     matrix[i].reverse();
-}
-console.log(matrix)
+    // console.log(matrix[i].length);
+  }
 };
 
 rotate([
